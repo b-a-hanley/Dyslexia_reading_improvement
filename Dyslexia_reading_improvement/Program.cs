b2b47bf048fba17b0c25913e0b4b5c955ee52508
b-a-hanley <benhanley321@gmail.com>
2023-06-27 00:13:13 +0100

    fixed bug level bug

--- Dyslexia_reading_improvement/Program.cs
+++ Dyslexia_reading_improvement/Program.cs
@@ -6,7 +6,7 @@ namespace Dyslexia_reading_improvement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the dyslexia reading improvement system \n************************************************** ");
+            Console.WriteLine("Welcome to the HyperLexic \n************************************************** ");
             Thread.Sleep(500);
             Console.WriteLine("I'm your host Bennington and heres the instructions..");
             Thread.Sleep(500);
@@ -57,98 +57,94 @@ namespace Dyslexia_reading_improvement
                 }
                 
             }
-           
-            
             void highscore()
             {
 
             }
-
-
-
             void game()
             {
-                
-                points = 5;
-
-                string[] words = { "birch", "because", "brilliant", "jamaican", "cordroy", "lemon", "trickled", "impeach", "establish", "configure", "number", "monosyllabic" };
-                Random rnd = new Random();
-                string word = words[rnd.Next(words.Length - 1)].ToUpper();
-                string randchar(int len)
+                bool gamestate = true;
+                while (gamestate)
                 {
-                    string str = "";
-                    for (int i = 0; i < len; i++)
-                    { str += Convert.ToChar(rnd.Next(97, 122)); }
-                    return str;
-                }
-                Console.WriteLine(word[0] + randchar(word.Length - 2) + word[word.Length - 1]);
-                string response = Console.ReadLine().ToUpper().Trim();
-                string ClueCut(string clue)
-                {
-                    string[] array = clue.ToUpper().Trim().Split("CLUE");
-                    try
-                    { return array[1]; }
-                    catch
-                    { return array[0]; }
+                    points = 5;
 
-                }
-                string clue = ClueCut(response);
-                while (response != clue)
-                {
-                    response = ClueCut(response);
-
-                    if ((clue == "1") && (points == 5))
+                    string[] words = { "birch", "because", "brilliant", "jamaican", "cordroy", "lemon", "trickled", "impeach", "establish", "configure", "number", "monosyllabic" };
+                    Random rnd = new Random();
+                    string word = words[rnd.Next(words.Length - 1)].ToUpper();
+                    string randchar(int len)
                     {
-                        Console.WriteLine(word[0].ToString() + word[1].ToString() + randchar(word.Length - 3).ToString() + word[word.Length - 1].ToString());
-                        response = Console.ReadLine();
-                        clue = ClueCut(response);
-
-                        points = 3;
+                        string str = "";
+                        for (int i = 0; i < len; i++)
+                        { str += Convert.ToChar(rnd.Next(97, 122)); }
+                        return str;
                     }
-                    else if ((clue == "2") && (points > 2))
+                    Console.WriteLine(word[0] + randchar(word.Length - 2) + word[word.Length - 1]);
+                    string response = Console.ReadLine().ToUpper().Trim();
+                    string ClueCut(string clue)
                     {
-                        Console.WriteLine(word[0].ToString() + word[1].ToString() + randchar(word.Length - 4).ToString() + word[word.Length - 2].ToString() + word[word.Length - 1].ToString());
-                        response = Console.ReadLine();
-                        clue = ClueCut(response);
-                        points = 2;
+                        string[] array = clue.ToUpper().Trim().Split("CLUE");
+                        try
+                        { return array[1]; }
+                        catch
+                        { return array[0]; }
                     }
-                    else if ((clue == "3") && (points > 1))
+                    string clue = ClueCut(response);
+                    while (response != clue)
                     {
-                        Console.WriteLine(word[0].ToString() + word[1].ToString() + word[2].ToString() + randchar(word.Length - 5).ToString() + word[word.Length - 2].ToString() + word[word.Length - 1].ToString());
-                        response = Console.ReadLine();
-                        clue = ClueCut(response);
-                        points = 1;
-                        break;
+                        response = ClueCut(response);
+
+                        if ((clue == "1") && (points == 5))
+                        {
+                            Console.WriteLine(word[0].ToString() + word[1].ToString() + randchar(word.Length - 3).ToString() + word[word.Length - 1].ToString());
+                            response = Console.ReadLine();
+                            clue = ClueCut(response);
+
+                            points = 3;
+                        }
+                        else if ((clue == "2") && (points > 2))
+                        {
+                            Console.WriteLine(word[0].ToString() + word[1].ToString() + randchar(word.Length - 4).ToString() + word[word.Length - 2].ToString() + word[word.Length - 1].ToString());
+                            response = Console.ReadLine();
+                            clue = ClueCut(response);
+                            points = 2;
+                        }
+                        else if ((clue == "3") && (points > 1))
+                        {
+                            Console.WriteLine(word[0].ToString() + word[1].ToString() + word[2].ToString() + randchar(word.Length - 5).ToString() + word[word.Length - 2].ToString() + word[word.Length - 1].ToString());
+                            response = Console.ReadLine();
+                            clue = ClueCut(response);
+                            points = 1;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You deserve to die in a pit of slimed eels");
+                        }
+                    }
+                    if (response == word)
+                    {
+                        Console.WriteLine("Well done");
+                        score += points;
+                        Console.WriteLine($"\nScore={score}\nLevel={level}\n");
+                        level++;
                     }
                     else
                     {
-                        Console.WriteLine("You deserve to die in a pit of slimed eels");
-
-                    }
-                }
-                if (response == word)
-                {
-                    Console.WriteLine("Well done");
-                    score += points;
-                    Console.WriteLine($"\nScore={score}\nLevel={level}\n");
-                    level++;
-                }
-                else
-                {
-
-                    Console.WriteLine($"The word was {word}\nYou lost all your points :(");
-                    Console.Write($"\nYour final score was");
-                    for (int i = 0; i < 3; i++)
-                    {
+                        Console.WriteLine($"The word was {word}\nYou lost all your points :(");
+                        Console.Write($"\nYour final score was");
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Thread.Sleep(300);
+                            Console.Write(".");
+                        }
                         Thread.Sleep(300);
-                        Console.Write(".");
-                    }
-                    Thread.Sleep(300);
-                    Console.WriteLine($"\nScore={score}\nLevel={level}\n");
+                        Console.WriteLine($"\nScore={score}\nLevel={level}\n");
 
-                    score = 0;
-                    level = 1;
-                    
+                        score = 0;
+                        level = 1;
+                        gamestate = false;
+
+                    }
                 }
             }
         }
