799f2dfd47375b261ac0e4127b41984e0990524e
b-a-hanley <benhanley321@gmail.com>
2023-06-26 23:55:47 +0100

    Improved the game loop

--- Dyslexia_reading_improvement/Program.cs
+++ Dyslexia_reading_improvement/Program.cs
@@ -19,15 +19,58 @@ namespace Dyslexia_reading_improvement
             //third time correct = 1 point
             //any time wrong = 0 points
             //makes an array of words
-            
+
             //random number generator 
-            
-            
             string playagain = ("y");
-            while (playagain=="y")
+            game();
+            while (playagain != "n")
             {
-                points = 5;
+                Console.WriteLine($"\nPlay again? (y/n) or for highscores type 'h'");
+                playagain = Console.ReadLine();
+                switch (playagain)
+                {
+                    case "y":
+                        game();
+                        break;
+                    case "yes":
+                        game();
+                        break;
+                    case "highscore":
+                        highscore();
+                        break;
+                    case "h":
+                        highscore();
+                        break;
+                    default:
+                        Console.Write("Thanks for coming i was your host bennington and this was ");
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Thread.Sleep(300);
+                            Console.Write(".");
+                        }
+                        Thread.Sleep(500);
+                        Console.WriteLine(" HyperLexic!");
+                       
+                        
+                        Environment.Exit(0);
+                        break;
+                }
                 
+            }
+           
+            
+            void highscore()
+            {
+
+            }
+
+
+
+            void game()
+            {
+                
+                points = 5;
+
                 string[] words = { "birch", "because", "brilliant", "jamaican", "cordroy", "lemon", "trickled", "impeach", "establish", "configure", "number", "monosyllabic" };
                 Random rnd = new Random();
                 string word = words[rnd.Next(words.Length - 1)].ToUpper();
@@ -35,26 +78,26 @@ namespace Dyslexia_reading_improvement
                 {
                     string str = "";
                     for (int i = 0; i < len; i++)
-                    { str += Convert.ToChar(rnd.Next(97, 122)); }                    
+                    { str += Convert.ToChar(rnd.Next(97, 122)); }
                     return str;
                 }
                 Console.WriteLine(word[0] + randchar(word.Length - 2) + word[word.Length - 1]);
                 string response = Console.ReadLine().ToUpper().Trim();
                 string ClueCut(string clue)
                 {
-                    string [] array = clue.ToUpper().Trim().Split("CLUE");
+                    string[] array = clue.ToUpper().Trim().Split("CLUE");
                     try
                     { return array[1]; }
                     catch
                     { return array[0]; }
-                    
+
                 }
                 string clue = ClueCut(response);
-                while (response!=clue)
+                while (response != clue)
                 {
                     response = ClueCut(response);
 
-                    if ((clue=="1")&&(points==5))
+                    if ((clue == "1") && (points == 5))
                     {
                         Console.WriteLine(word[0].ToString() + word[1].ToString() + randchar(word.Length - 3).ToString() + word[word.Length - 1].ToString());
                         response = Console.ReadLine();
@@ -62,25 +105,25 @@ namespace Dyslexia_reading_improvement
 
                         points = 3;
                     }
-                    else if ((clue=="2")&& (points>2 ))
+                    else if ((clue == "2") && (points > 2))
                     {
                         Console.WriteLine(word[0].ToString() + word[1].ToString() + randchar(word.Length - 4).ToString() + word[word.Length - 2].ToString() + word[word.Length - 1].ToString());
                         response = Console.ReadLine();
                         clue = ClueCut(response);
                         points = 2;
                     }
-                    else if ((clue == "3")&&(points>1))
+                    else if ((clue == "3") && (points > 1))
                     {
                         Console.WriteLine(word[0].ToString() + word[1].ToString() + word[2].ToString() + randchar(word.Length - 5).ToString() + word[word.Length - 2].ToString() + word[word.Length - 1].ToString());
                         response = Console.ReadLine();
                         clue = ClueCut(response);
                         points = 1;
-                        break;break;
+                        break;
                     }
                     else
                     {
                         Console.WriteLine("You deserve to die in a pit of slimed eels");
-                        
+
                     }
                 }
                 if (response == word)
@@ -95,25 +138,19 @@ namespace Dyslexia_reading_improvement
 
                     Console.WriteLine($"The word was {word}\nYou lost all your points :(");
                     Console.Write($"\nYour final score was");
-                    Thread.Sleep(1000);
-                    Console.Write(".");
-                    Thread.Sleep(500);
-                    Console.Write(".");
-                    Thread.Sleep(300);
-                    Console.Write(".");
+                    for (int i = 0; i < 3; i++)
+                    {
+                        Thread.Sleep(300);
+                        Console.Write(".");
+                    }
                     Thread.Sleep(300);
                     Console.WriteLine($"\nScore={score}\nLevel={level}\n");
 
                     score = 0;
                     level = 1;
-                    Console.WriteLine($"\nPlay again? (y/n) or for highscores type 'h'");
-                    playagain=Console.ReadLine();
-                     
+                    
                 }
-                
-
             }
-            
         }
     }
 }
